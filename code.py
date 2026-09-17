35666775da1b3887873942d9d6e1eeeae520dcf2
Vinay Gupta <vinay.gupta@xebia.com>
2026-09-17 12:26:41 +0530

    Revert "Add cube function"
    This reverts commit d1de3cc25ecc43efa124a8f78e0b0307970d5745.

--- code.py
+++ code.py
@@ -182,14 +182,6 @@ def safe_eval(expr: str):
     return _eval(parsed)
 
 
-def cube(n):
-    """Return the cube of n."""
-    return n ** 3
-
-# Register cube in the allowed functions so safe_eval can call it
-_ALLOWED_FUNCS['cube'] = cube
-
-
 def _print_welcome():
     print("Simple Safe Calculator")
     print("Type arithmetic expressions to evaluate. Examples:")
@@ -203,7 +195,7 @@ def _print_welcome():
 
 def _print_help():
     print("Available functions:")
-    names =[REDACTED]
+    names = sorted(list(_ALLOWED_FUNCS.keys()) + list(_ALLOWED_NAMES.keys()))
     print(', '.join(names))
 
 
